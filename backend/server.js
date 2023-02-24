e24fbf0dbb4a4172fe595398e4e2c5dc4eb6ef97
rathodgautam <104826264+rathodgautam@users.noreply.github.com>
2023-02-25 01:45:02 +0530

    build added

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1,  "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
