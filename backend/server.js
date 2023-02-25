b25e00cb4d66f5b0274e0290ea02e91e23f87883
rathodgautam <104826264+rathodgautam@users.noreply.github.com>
2023-02-25 15:23:59 +0530

    origin  origin:'https://vibechat-on.onrender.com',

--- backend/server.js
+++ backend/server.js
@@ -52,13 +52,13 @@ const PORT = process.env.PORT|| 5000;
 //  const allowedOrigins = ['https://vibechat-on.onrender.com'];
 
  app.use(cors({
-  origin:'*',
+  origin:'https://vibechat-on.onrender.com',
 }));
 
  const io = require("socket.io")(server, {
     pingTimeout : 60000,
     cors : {
-      origin:'*',
+     origin:'https://vibechat-on.onrender.com',
   },
  });
 
