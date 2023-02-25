da59ec59cc8274634ad12d165945113ce2e20c6a
rathodgautam <104826264+rathodgautam@users.noreply.github.com>
2023-02-25 23:25:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT;
  const server = app.listen(PORT, console.log(`Server Started on PORT http://localhost:${PORT}`))
 
 
