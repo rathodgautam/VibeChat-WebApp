d6604fbb40f7ed6f07861b8a59e2c19f4a391a3f
rathodgautam <104826264+rathodgautam@users.noreply.github.com>
2023-02-25 14:45:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -51,11 +51,13 @@ const PORT = process.env.PORT|| 5000;
 //   origin: 'http://localhost:3000/'
 // }));
 
+ const allowedOrigins = ['httphttp://localhost:3000','https://vibechat-on.onrender.com'];
+
  const io = require("socket.io")(server, {
     pingTimeout : 60000,
     cors : {
-        origin: 'http://localhost:3000',
-    },
+      origin: allowedOrigins,
+  },
  });
 
  io.on("connection", (socket) => {
